a49fe0e757a9c0d67cdf16c665c483814d900ce9
bayonAtpark <bayon.forte@parkseed.com>
2017-10-12 08:04:38 -0400

    integrated angular and php mysql api data

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-var app = angular.module('app',[]);
+//initial angular module
+var app = angular.module('app',["ngRoute"]);
+
+//routing
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "main.html"
+    })
+    .when("/green", {
+        templateUrl : "green.html"
+    })
+    .when("/red", {
+        templateUrl : "red.html"
+    });
+});
